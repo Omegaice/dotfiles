6e2421dd461577aac3ea14c07e3da73fde60086b
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-01-21 11:38:40 -0500

    feat(power): some power configuration changes

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -12,22 +12,22 @@
   };
 
   services.tlp = {
-    enable = false;
+    enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MAX_PERF_ON_BAT = 80;
 
       #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      START_CHARGE_THRESH_BAT0 = 90; # 40 and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 100; # 80 and above it stops charging
 
       # Intel GPU
       INTEL_GPU_MIN_FREQ_ON_AC = 500;
