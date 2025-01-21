f111c613a6bf2da41b64f8c30bec3004b4bb5f26
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-01-21 11:37:26 -0500

    feat(steam): firewall and gamemode settings

--- system/programs/gaming/steam.nix
+++ system/programs/gaming/steam.nix
@@ -1,6 +1,16 @@
-{
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+
+    # Firewall Settings
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+
+    package = pkgs.steam.override {
+      extraPkgs = (pkgs: with pkgs; [
+        gamemode
+      ]);
+    };
   };
 }
