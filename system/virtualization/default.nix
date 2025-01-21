c11ecd9032bdc385f123b9af1f5a04c7e22e7972
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-01-21 11:37:43 -0500

    feat(qemu): qemu virtual machine setup

---
+++ system/virtualization/default.nix
@@ -0,0 +1,20 @@
+{pkgs}: {
+  environment.systemPackages = with pkgs; [
+    virt-manager
+  ];
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+
+      qemu = {
+        package = pkgs.qemu_kvm;
+        ovmf = {
+          enable = true;
+          packages = [pkgs.OVMFFull.fd];
+        };
+        swtpm.enable = true;
+      };
+    };
+  };
+}
